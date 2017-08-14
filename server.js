b5bd43baa9fbcca6bca8384b83eeafa9eb08e7cd
kamoltip <plawansusu@gmail.com>
2017-08-14 08:26:03 -0400

    remove pack

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+// app.use("/update", routes);
+// app.use("/create", routes);
 
 	app.listen(port, function(){
 		console.log("listen to", port);
