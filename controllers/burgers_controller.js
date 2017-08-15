241434bfb795933b442edbe8e10d5cb9cc39c64e
kamoltip <plawansusu@gmail.com>
2017-08-15 00:12:00 -0400

    jawsdb

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,11 +6,11 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
-// router.get("/",function(req, res){
-//   res.redirect("/burgers");
-// });
+router.get("/",function(req, res){
+  res.redirect("/burgers");
+});
 
-router.get("/", function(req, res) {
+router.get("/burgers", function(req, res) {
   burger.all(function(burgerData) {
       res.render("index", {burger_data: burgerData});   
   });
